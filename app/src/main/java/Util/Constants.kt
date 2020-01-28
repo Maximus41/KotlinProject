ee08a4a2e66c0ecd2a8b84ce2ffe5be091e1e09d
Maximus41 <agnideepp@gmail.com>
2020-01-29 04:38:54 +0530

    Added a constant file

---
+++ app/src/main/java/Util/Constants.kt
@@ -0,0 +1,13 @@
+package Util
+
+const val BASE_URL = "https://5e27f48d6eeb44001453708b.mockapi.io/api/gallery/"
+
+const val CONNECTION_TIME_OUT = 10000
+
+const val PAGE_SIZE = 10
+
+const val APPLICATION_TAG = "Kotlin Test App"
+
+const val TOTAL_ITEM_COUNT = 100
+
+const val TOTAL_PAGE = TOTAL_ITEM_COUNT / PAGE_SIZE
